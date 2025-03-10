8c1353fd8051ffa6d4e1b7cf4f4b16759bd9c9d7
Silvan <38138071+Sillyvan@users.noreply.github.com>
2025-03-10 19:02:37 +0100

    migrate to worker assets

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
 	import '../app.css';
+	import { locales, localizeHref } from '$lib/paraglide/runtime';
+  import { page } from '$app/state';
+
 
 	let { children } = $props();
 </script>
+
+<div style="display:none">
+	{#each locales as locale}
+		<a href={localizeHref(page.url.pathname, { locale })}>{locale}</a>
+	{/each}
+</div>
 
 <div class="app">
 	<main>
